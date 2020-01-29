fbf029d72c2ab9eade195270aa213215936eb162
Jiashu XU <1999J0615une@gmail.com>
2020-01-28 17:48:29 -0800

    commit at 28/01/2020 17:48:29

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 x='neovim neofetch xclip krusader mc doublecmd-qt5 cmus redshift termite plasma-browser-integration ranger
     andromeda-wallpaper plasma5-themes-andromeda sddm-andromeda-theme andromeda-icon-theme 
-    speedtest-cli timeshift cmake gdb zathura-pdf-mupdf wmctrl '
+    speedtest-cli timeshift cmake gdb zathura-pdf-mupdf wmctrl flameshot'
 y='qtpad-git clion webstorm intellij-idea-ultimate-edition pycharm-professional autokey cmatrix'
 sudo pacman -Syu;
 sudo pacman --needed -S ${x}
