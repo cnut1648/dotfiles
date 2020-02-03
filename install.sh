9a3a27f8cfc58bffa71e543a340e4baed9ef5717
Jiashu XU <1999J0615une@gmail.com>
2020-02-03 00:08:23 -0800

    commit at 03/02/2020 00:08:23

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-x='neovim neofetch xclip krusader mc doublecmd-qt5 cmus redshift termite plasma-browser-integration ranger
-    andromeda-wallpaper plasma5-themes-andromeda sddm-andromeda-theme andromeda-icon-theme 
+x='neovim neofetch xclip krusader mc doublecmd-qt5 cmus redshift termite plasma-browser-integration ranger xorg-xev xorg-xrandr
+    andromeda-wallpaper plasma5-themes-andromeda sddm-andromeda-theme andromeda-icon-theme xdotool 
     speedtest-cli timeshift cmake gdb zathura-pdf-mupdf wmctrl flameshot'
 y='qtpad-git clion webstorm intellij-idea-ultimate-edition pycharm-professional autokey cmatrix'
 sudo pacman -Syu;
