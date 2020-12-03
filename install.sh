a5b46799fc8c4d2ec80aafcbbb5a2de3b6f3379f
Jiashu XU <1999J0615une@gmail.com>
2020-12-03 10:11:09 -0800

    commit at 03/12/2020 10:11:09

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 x='neovim neofetch xclip doublecmd-qt5 cmus redshift kitty kitty-terminfo plasma-browser-integration ranger xorg-xev xorg-xrandr
     andromeda-wallpaper plasma5-themes-andromeda sddm-andromeda-theme andromeda-icon-theme xdotool mariadb youtube-dl ncdu xcape
     speedtest-cli timeshift cmake gdb zathura-pdf-mupdf zathura-djvu wmctrl flameshot fzf nodejs npm jq python-jedi python-pynvim ripgrep yarn emacs texlive-most tomcat8 gradle maven
-    fd clementine boost'
+    fd clementine boost racket python-virtualenv'
 y='qtpad-git clion webstorm intellij-idea-ultimate-edition pycharm-professional autokey cmatrix rstudio-desktop-bin autotiling geeqie mycli xcwd lazygit vmware-station calibre-python3 postman arduino arduino-avr-core python-i3-grid'
 sudo pacman -Syu;
 sudo pacman --needed -S ${x}
