fe72b785cc8e4a0d0d2150c3366b99ab25794990
jiashu <jiashuxu@usc.edu>
2021-02-22 14:30:20 -0800

    add global comments

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# global: gtags
 x='neovim neofetch xclip doublecmd-qt5 cmus redshift kitty kitty-terminfo plasma-browser-integration ranger feh xorg-xev xorg-xrandr docker docker-compose fasd manjaro-i3-settings xdotool mariadb youtube-dl ncdu xcape graphviz valgrind speedtest-cli timeshift cmake gdb zathura-pdf-mupdf zathura-djvu wmctrl flameshot fzf nodejs npm jq python-jedi python-pynvim ripgrep yarn emacs texlive-most tomcat8 gradle maven fd clementine boost racket python-virtualenv ibus ibus-libpinyin ctags pacui mlocate cmake unrar global'
 
 # kite is installer, need ./kite-installer install
