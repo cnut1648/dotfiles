b138e6631572d4c3c0eadad8060579224f22c40b
jiashu <jiashuxu@usc.edu>
2021-04-16 16:39:52 -0700

    add

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 # global: gtags
-x='neovim neofetch xclip doublecmd-qt5 cmus redshift kitty kitty-terminfo plasma-browser-integration ranger feh xorg-xev xorg-xrandr docker docker-compose fasd manjaro-i3-settings xdotool mariadb youtube-dl ncdu xcape graphviz valgrind speedtest-cli timeshift cmake gdb zathura-pdf-mupdf zathura-djvu wmctrl flameshot fzf nodejs npm jq python-jedi python-pynvim ripgrep yarn emacs texlive-most tomcat8 gradle maven fd clementine boost racket python-virtualenv ibus ibus-libpinyin ctags pacui mlocate cmake unrar global'
+# postgis: geographic support for postgresql
+x='neovim neofetch xclip doublecmd-qt5 cmus redshift kitty kitty-terminfo plasma-browser-integration ranger feh xorg-xev xorg-xrandr docker docker-compose fasd manjaro-i3-settings xdotool mariadb youtube-dl ncdu xcape graphviz valgrind speedtest-cli timeshift cmake gdb zathura-pdf-mupdf zathura-djvu wmctrl flameshot fzf nodejs npm jq python-jedi python-pynvim ripgrep yarn emacs texlive-most tomcat8 gradle maven fd clementine boost racket python-virtualenv ibus ibus-libpinyin ctags pacui mlocate cmake unrar global postgresql postgis'
 
 # kite is installer, need ./kite-installer install
-y='qtpad-git typora clion webstorm intellij-idea-ultimate-edition pycharm-professional autokey-qt chromium cmatrix rstudio-desktop-bin autotiling geeqie mycli xcwd-git lazygit calibre-git postman-bin arduino arduino-avr-core python-i3-grid kite joplin jmeter zotero joplin-desktop ibus-qt visual-studio-code-bin wireshark-qt'
+# r2cutter: Qt for cutter, reverse engineering
+# r2ghidra: r2cutter's decompiler plugin
+y='qtpad-git typora clion webstorm intellij-idea-ultimate-edition datagrip pycharm-professional autokey-qt chromium cmatrix rstudio-desktop-bin autotiling geeqie mycli xcwd-git lazygit calibre-git postman-bin arduino arduino-avr-core python-i3-grid kite joplin jmeter zotero joplin-desktop ibus-qt visual-studio-code-bin wireshark-qt r2cutter r2ghidra gurobi'
 
 
 sudo pacman -Syu;
