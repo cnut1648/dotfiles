aa1d9dc73422fbb9c0b32fce94b7cba823c4c739
jiashu <jiashuxu@usc.edu>
2021-07-22 14:06:21 -0700

    add

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 x='neovim neofetch xclip doublecmd-qt5 cmus redshift kitty kitty-terminfo plasma-browser-integration ranger feh xorg-xev xorg-xrandr docker docker-compose fasd manjaro-i3-settings xdotool mariadb youtube-dl ncdu xcape graphviz valgrind speedtest-cli timeshift cmake gdb zathura-pdf-mupdf zathura-djvu wmctrl flameshot fzf nodejs npm jq python-jedi python-pynvim ripgrep yarn emacs tomcat8 gradle maven fd clementine boost python-virtualenv ibus ibus-libpinyin ctags pacui mlocate cmake unrar global postgresql postgis scala sbt tree ammonite'
 
 # kite is installer, need ./kite-installer install
-y='qtpad-git typora clion webstorm intellij-idea-ultimate-edition datagrip pycharm-professional autokey-qt chromium cmatrix rstudio-desktop-bin autotiling geeqie mycli xcwd-git lazygit calibre postman-bin python-i3-grid kite joplin jmeter zotero joplin-desktop ibus-qt visual-studio-code-bin wireshark-qt gurobi apache-spark rslsync activitywatch-bin logseq-desktop obsidian firebase-tools'
+# gurobi: python linear programming
+# fava: UI for beancount
+y='qtpad-git typora clion webstorm intellij-idea-ultimate-edition datagrip pycharm-professional autokey-qt chromium cmatrix rstudio-desktop-bin autotiling geeqie mycli xcwd-git lazygit calibre postman-bin python-i3-grid kite joplin jmeter zotero joplin-desktop ibus-qt visual-studio-code-bin wireshark-qt gurobi apache-spark rslsync activitywatch-bin logseq-desktop obsidian firebase-tools beancount fava'
 
 
 # qemu: simulator required for xv6
