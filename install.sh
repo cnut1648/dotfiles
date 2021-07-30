8bedee1840ae63c67f2e535c91cc65da8bfc6d89
jiashu <jiashuxu@usc.edu>
2021-07-29 20:46:52 -0700

    add

--- install.sh
+++ install.sh
@@ -1,21 +1,108 @@
-# global: gtags
-# postgis: geographic support for postgresql
-x='neovim neofetch xclip doublecmd-qt5 cmus redshift kitty kitty-terminfo plasma-browser-integration ranger feh xorg-xev xorg-xrandr docker docker-compose fasd manjaro-i3-settings xdotool mariadb youtube-dl ncdu xcape graphviz valgrind speedtest-cli timeshift cmake gdb zathura-pdf-mupdf zathura-djvu wmctrl flameshot fzf nodejs npm jq python-jedi python-pynvim ripgrep yarn emacs tomcat8 gradle maven fd clementine boost python-virtualenv ibus ibus-libpinyin ctags pacui mlocate cmake unrar global postgresql postgis scala sbt tree ammonite'
+x=(
+    neovim neofetch xclip doublecmd-qt5 cmus redshift kitty kitty-terminfo plasma-browser-integration ranger
+    feh xorg-xev xorg-xrandr docker docker-compose fasd manjaro-i3-settings xdotool mariadb youtube-dl
+    # disk usage in command line, replace du
+    ncdu
+    xcape graphviz valgrind speedtest-cli timeshift cmake gdb
+    # fast pdf reader
+    zathura-pdf-mupdf zathura-djvu
+    wmctrl flameshot fzf nodejs npm jq
+    # python support for nvim
+    python-jedi python-pynvim
+    # replace grep
+    ripgrep
+    yarn emacs tomcat8 gradle maven
+    # better find
+    fd
+    # music player
+    clementine
+    boost
+    # virtualenv -p /path/to/python venv
+    python-virtualenv
+    # ibus and chinese input method
+    ibus ibus-libpinyin
+    # analyze pacman
+    pacui mlocate cmake unrar
+    # global: nvim gtags needed
+    # ctags: better gtags in nvim
+    global ctags
+    # amm, ipython for scala
+    scala sbt  ammonite
+    # plain text tree
+    tree
+    # ls with icon
+    # lsd --tree = tree with icon
+    lsd
+    # cat with syntax highlight
+    bat
+)
 
-# kite is installer, need ./kite-installer install
+# kite is
 # gurobi: python linear programming
 # fava: UI for beancount
-y='qtpad-git typora clion webstorm intellij-idea-ultimate-edition datagrip pycharm-professional autokey-qt chromium cmatrix rstudio-desktop-bin autotiling geeqie mycli xcwd-git lazygit calibre postman-bin python-i3-grid kite joplin jmeter zotero joplin-desktop ibus-qt visual-studio-code-bin wireshark-qt gurobi apache-spark rslsync activitywatch-bin logseq-desktop obsidian firebase-tools beancount fava'
+y=(
+    qtpad-git typora
+    # Jetbrains
+    clion webstorm intellij-idea-ultimate-edition datagrip pycharm-professional
+    # OneQuick in Linux
+    autokey-qt
+    chromium cmatrix rstudio-desktop-bin
+    # i3wm auto tile windows if having many windows at same time
+    autotiling
+    # image viewer
+    geeqie
+    # better mysql CLI
+    mycli
+    # change dir, need for ranger
+    xcwd-git
+    lazygit calibre postman-bin
+    # support i3 grid
+    python-i3-grid
+    # installer, need ./kite-installer install
+    kite
+    joplin joplin-desktop
+    jmeter
+    zotero
+    # support UI in Qt for ibus
+    ibus-qt
+    visual-studio-code-bin
+    wireshark-qt gurobi
+    # spark-submit
+    apache-spark
+    rslsync
+    # monitor screen for daily activity
+    activitywatch-bin
+    # notes
+    logseq-desktop obsidian
+    # accounting
+    beancount fava
+    # firebase CLI
+    firebase-tools
+    # better Ctrl-R
+    # after install add in .zshrc
+    # eval "$(mcfly init zsh)"
+    # then Ctrl-R auto use mcfly
+    mcfly
+)
 
-
-# qemu: simulator required for xv6
-# r2cutter: Qt for cutter, reverse engineering
-# r2ghidra: r2cutter's decompiler plugin
-# racket: IDE for racket: DrRacket
-# avr: for compile arduino code (can without arduino IDE)
-# ida: free version only, hard to hack
-optional='android-emulator r2cutter r2ghidra qemu texlive-most racket arduino arduino-avr-core ida-free'
-
+optional=(
+    android-emulator
+    # r2cutter: Qt for cutter, reverse engineering
+    # r2ghidra: r2cutter's decompiler plugin
+    r2cutter r2ghidra
+    # simulator required for xv6
+    qemu
+    texlive-most
+    # IDE for racket: DrRacket
+    racket
+    # arduino: IDE
+    # arduino-avr-core: for compile arduino code (can without arduino IDE)
+    arduino arduino-avr-core
+    # free version only, hard to hack
+    ida-free
+    # geographic support for postgresql
+    postgresql postgis
+)
 
 sudo pacman -Syu;
 # sudo pacman --needed -S ${x}
